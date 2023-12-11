9bf9559b1c571eb80f77cf9694fe23e97053747c
keita-komoto <makibiso200x@yahoo.co.jp>
2023-12-11 22:21:14 +0900

    20231211

---
+++ src/Test.java
@@ -0,0 +1,35 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		Person taro = new Person();
+		taro.name="山田太郎";
+		taro.age=20;
+		
+		System.out.println(taro.name);
+		System.out.println(taro.age);
+		
+		taro.name="木村次郎";
+		taro.age=18;
+		
+		System.out.println(taro.name);
+		System.out.println(taro.age);
+		
+		taro.name = "鈴木花子";
+		taro.age = 16;
+		
+		System.out.println(taro.name);
+		System.out.println(taro.age);
+		
+		taro.name = "河本敬太";
+		taro.age = 32;
+		
+		System.out.println(taro.name);
+		System.out.println(taro.age);
+		
+		
+	}
+
+}
